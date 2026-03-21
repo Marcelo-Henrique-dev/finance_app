8b8d374f812ff66f9133d1c60fcafef2639cfb8d
Marcelo-Henrique-dev <dvmarcelohenrique@gmail.com>
2026-03-21 00:17:49 -0300

    Inicial Screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:finance_app/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -7,13 +8,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Home'
-          ),
-        ),
-      ),
+      home: SplashPage(),
     );
   }
 }
---
+++ lib/features/splash/splash_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF63B5AF),
+              Color(0xFF438883)
+            ]
+          )
+        ),
+        child: Text(
+          'finance',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 50,
+            fontWeight: FontWeight(700)
+          ),
+        ),
+      ),
+    );
+  }
+}
