634113c5c14a29d44bb456afc6fc3f6688de5c4d
Marcelo-Henrique-dev <dvmarcelohenrique@gmail.com>
2026-04-14 16:31:26 -0300

    some changes to better primaryButton

--- lib/common/widgets/primary_button.dart
+++ lib/common/widgets/primary_button.dart
@@ -3,44 +3,41 @@ import 'package:finance_app/common/constants/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class PirmaryButton extends StatelessWidget {
-
   final VoidCallback? onPressed;
   final String text;
 
-  const PirmaryButton({
-    super.key,
-    this.onPressed,
-    required this.text,
-  });
+  const PirmaryButton({super.key, this.onPressed, required this.text});
+
+  final BorderRadius _borderRadius = const BorderRadius.all(
+    Radius.circular(24.0),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      borderRadius: const BorderRadius.all(Radius.circular(38.0)),
+      borderRadius: _borderRadius,
       child: Ink(
+        height: 48,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(38.0)),
+          borderRadius: BorderRadius.all(Radius.circular(24.0)),
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: onPressed != null ? AppColors.greenGradient : AppColors.greyGradient
+            colors: onPressed != null
+                ? AppColors.greenGradient
+                : AppColors.greyGradient,
           ),
         ),
         child: InkWell(
-          borderRadius: const BorderRadius.all(Radius.circular(38.0)),
+          borderRadius: _borderRadius,
           onTap: () {
             onPressed;
           },
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(.circular(38.0))
-            ),
-            alignment: .center,
-            height: 64,
+          child: Align(
             child: Text(
               text,
               style: AppTextStyles.mediumText18.copyWith(
-                color: AppColors.white
+                color: AppColors.white,
               ),
             ),
           ),
