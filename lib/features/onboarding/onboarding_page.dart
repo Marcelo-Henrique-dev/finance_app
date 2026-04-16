3c32fa00e6361f28b2f074bd8b18e8bde29acfc9
Marcelo-Henrique-dev <dvmarcelohenrique@gmail.com>
2026-04-16 11:48:46 -0300

    textbutton created

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -1,8 +1,11 @@
-import 'package:finance_app/common/widgets/primary_button.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:finance_app/common/widgets/multi_text_button.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:finance_app/common/constants/app_colors.dart';
 import 'package:finance_app/common/constants/app_text_styles.dart';
+import 'package:finance_app/common/widgets/primary_button.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
@@ -17,9 +20,7 @@ class OnboardingPage extends StatelessWidget {
             child: Container(
               alignment: Alignment.bottomCenter,
               color: AppColors.iceWhite,
-              child: Image.asset(
-                'assets/images/man.png'
-              ),
+              child: Image.asset('assets/images/man.png'),
             ),
           ),
           Expanded(
@@ -27,28 +28,45 @@ class OnboardingPage extends StatelessWidget {
               color: AppColors.white,
               width: double.infinity,
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 30),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 0,
+                  vertical: 30,
+                ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Text(
                       'Spend Smarter \n Save More',
-                      style: AppTextStyles.mediumText.copyWith(color: AppColors.greenLigthTwo),
+                      style: AppTextStyles.mediumText.copyWith(
+                        color: AppColors.greenLigthTwo,
+                      ),
                       textAlign: TextAlign.center,
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: PirmaryButton(text: 'Get Starded', onPressed: (){},),
+                      child: PirmaryButton(
+                        text: 'Get Starded',
+                        onPressed: () {},
+                      ),
                     ),
-                    Text(
-                      'Already Habe Account? Log In',
-                      style: AppTextStyles.smallText.copyWith(color: AppColors.grey),
+                    MultiTextButton(
+                      children: [
+                        Text(
+                          'Already Have Account? ',
+                          style: AppTextStyles.smallText.copyWith(color: AppColors.grey),
+                        ),
+                        Text(
+                          'Log In',
+                          style: AppTextStyles.smallText.copyWith(color: AppColors.greenLigthTwo),
+                        )
+                      ],
+                      onPressed: () => print("Texto Clicado"),
                     ),
                   ],
                 ),
               ),
             ),
-          )
+          ),
         ],
       ),
     );
